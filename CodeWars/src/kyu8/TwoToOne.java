d3ab37a1c7caf9373a19d886586e64929a5546d3
ferero92 <ferero92@hotmail.com>
2018-02-23 21:30:42 +0100

    2018/02/23

---
+++ CodeWars/src/kyu8/TwoToOne.java
@@ -0,0 +1,42 @@
+package kyu8;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TwoToOne {
+    
+    public static String longest (String s1, String s2) {
+        
+        List<Character> list = new ArrayList<>();
+        list = containsList(list, s1);
+        list = containsList(list, s2);
+        
+        Collections.sort(list);
+        
+        s1 = toString(list);
+        
+        return s1;
+    }
+    
+    private static List<Character> containsList(List<Character> list, String string) {
+    	
+    	for (int i = 0; i < string.length(); i++) {
+    		if (!list.contains(string.charAt(i)))
+    			list.add(string.charAt(i));
+    	}
+    	
+    	return list;
+    }
+    
+    private static String toString(List<Character> list) {
+    	String result = new String();
+    	
+    	for (Character c : list) {
+    		result += c;
+    	}
+    	
+    	return result;
+    }
+
+}
